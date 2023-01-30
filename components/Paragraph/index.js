f9fbc33d682a1c6915dff91e93398c3018740493
eoss-sh <sh@eoss.ch>
2023-01-30 15:50:51 +0100

    update

--- components/Paragraph/index.js
+++ components/Paragraph/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { getTextAlign, getFontColor } from "utils/font";
+import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteUrls";
 
 export const Paragraph = ({ content, textAlign, color }) => {
   const tag = React.createElement("p", {
-    dangerouslySetInnerHTML: { __html: content },
+    dangerouslySetInnerHTML: { __html: relativeToAbsoluteUrls(content) },
     className: `font-body max-w-5xl mx-auto my-auto ${getTextAlign(
       textAlign
     )} ${getFontColor(color)}`,
